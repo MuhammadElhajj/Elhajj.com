c7027c0320dd16a93c0134879466d82e0530c205
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2024-12-24 05:35:12 -0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css'
 import Home from "./Home/Home";
 import Header from './Header/Header';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import ControlPanel from './ControlPanel/ControlPanel';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ControlPanel />
       <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
