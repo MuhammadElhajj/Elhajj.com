72ce3104bd02765ea649800aa48cbd4939707ed6
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2024-12-24 05:28:48 -0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
 import Home from "./Home/Home";
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
